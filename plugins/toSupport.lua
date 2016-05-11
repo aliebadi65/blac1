c8810372eb045031f9c78a8b587bd3f6e7372b30
aliebadi65 <aliebadi66@gmail.com>
2016-05-11 14:09:21 +0430

    Update toSupport.lua

--- plugins/toSupport.lua
+++ plugins/toSupport.lua
@@ -1,8 +1,7 @@
 do
 
 function run(msg, matches)
-  return " The Support Invition Link : \n 
-https://telegram.me/joinchat/C2AZVz9e1oKPUz8OUpEUew\n-------------------------------------\nID: @AliE1_bot"\n\ Channel: @smartgp
+  return " The Support Invition Link : \n https://telegram.me/joinchat/C2AZVz9e1oKPUz8OUpEUew\n-------------------------------------\nID: @smartgp"
   end
 return {
   description = "shows support link", 
